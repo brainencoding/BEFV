5b3d5c19ad9798b73ccdd3f023f6e21a9fcd4c17
Adamets Vladislav <v.adamets@consult-info.ru>
2021-06-14 21:48:59 +0200

    upd: create class for custom Exception

---
+++ src/core/Exception.ts
@@ -0,0 +1,7 @@
+import {BEFormValidator} from "../index";
+
+export class Exception{
+	static throw(s: string, _class: string = 'BEFormValidator') {
+		return BEFormValidator.__utils.errorConstructor(_class)(s)
+	}
+}
