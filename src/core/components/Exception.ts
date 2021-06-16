0bf11a654b658ff09acec6ea93c5b658ff2ca56d
Adamets Vladislav <v.adamets@consult-info.ru>
2021-06-17 00:24:27 +0200

    upd

--- src/core/components/Exception.ts
+++ src/core/components/Exception.ts
@@ -1,7 +1,14 @@
 import {BEFormValidator} from "../../index";
 
 export class Exception{
-	static throw(s: string, _class: string = 'BEFormValidator') {
+	/**
+	 * Exception::Error creator
+	 *
+	 * @param s {String}
+	 * @param _class {String}
+	 * @return {Error}
+	 * */
+	static throw(s: string, _class: string = 'BEFormValidator'): Error {
 		return BEFormValidator.__utils.errorConstructor(_class)(s)
 	}
 }
