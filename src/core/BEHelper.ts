7a9a9dd51218016325c85a2e87dfbe8664200def
Vladislav Adamets <v.adamets@consult-info.ru>
2021-10-29 14:22:16 +0200

    chore: copy arrayEquals method of BEUtils

--- src/core/BEHelper.ts
+++ src/core/BEHelper.ts
@@ -20,4 +20,65 @@ export class BEHelper {
 
         return uuid;
     }
+
+    // @ts-ignore
+    static arrayEquals(source, comparable) {
+        if (!source && !comparable) {
+            return false;
+        }
+
+        if (source.length !== comparable.length) {
+            return false;
+        }
+
+        for (let i = 0, l = source.length; i < l; i++) {
+            if (source[i] instanceof Array && comparable[i] instanceof Array) {
+                if (!BEHelper.arrayEquals(source[i], comparable[i])) {
+                    return false;
+                }
+            } else if (source[i] !== comparable[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // @ts-ignore
+    static objectEquals(source, comparable) {
+        for (let propName in source) {
+            if (source.hasOwnProperty(propName) !== comparable.hasOwnProperty(propName)) {
+                return false;
+            }
+
+            else if (typeof source[propName] !== typeof comparable[propName]) {
+                return false;
+            }
+        }
+
+        for (let propName in comparable) {
+            if (source.hasOwnProperty(propName) !== comparable.hasOwnProperty(propName)) {
+                return false;
+            } else if (typeof source[propName] !== typeof comparable[propName]) {
+                return false;
+            }
+
+            if (!source.hasOwnProperty(propName)) {
+                continue;
+            }
+
+            if (source[propName] instanceof Array && comparable[propName] instanceof Array) {
+                if (!BEHelper.arrayEquals(source[propName], comparable[propName])) {
+                    return false;
+                }
+            } else if (source[propName] instanceof Object && comparable[propName] instanceof Object) {
+                if (!BEHelper.objectEquals(source[propName], comparable[propName])) {
+                    return false;
+                }
+            } else if (source[propName] !== comparable[propName]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
